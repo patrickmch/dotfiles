63e57dcd293e97c52895ff5310aa77f25b04162d
patrickmch <patrickmcheyser@gmail.com>
2017-10-23 09:39:51 -0600

    Adds fonts and a script to install them

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 # WIP
 
 # install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # install npm
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# brew tap caskroom/fonts
+# brew cask install font-hack-nerd-font
 
-
-# install nerd-fonts for powerline stuff
-brew tap caskroom/fonts
-brew cask install font-hack-nerd-font
+# install fonts (copy them to Font directory)
+cp -R ~/dotfiles/fonts/. ~/Library/Fonts
